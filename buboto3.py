086ff1dfaf9915bf41966c56a75753a34fd7e95d
Albin Antony <aantony@redhat.com>
2019-01-31 16:11:18 +0530

    Update buboto3.py

--- buboto3.py
+++ buboto3.py
@@ -1,25 +1,18 @@
-import hashlib
-import boto3
+#import hashlib
+#import boto3
 
-access_key= "MG17R69AX5JRF9NIE996"
-secret_key= "82DoovmUtAUdQnbmn20SAdgsdeA7ZVCpYTmrL8UV"
+#access_key= "MG17R69AX5JRF9NIE996"
+#secret_key= "82DoovmUtAUdQnbmn20SAdgsdeA7ZVCpYTmrL8UV"
 
-client = boto3.client('s3',
-aws_access_key_id=access_key,
-aws_secret_access_key=secret_key,
-endpoint_url='http://172.16.115.75:8080',
-region_name='',
-)
-
-
-bucket = client.create_bucket(Bucket='sts_bkt2')
-#bucket = client.create_bucket(Bucket='sts_bkt3')
-response1 = client.list_objects_v2(Bucket='sts_bkt2')
-"""
-for bucket in response["Buckets"]:
-        print "{name}\t{created}".format(
-                    name = bucket['Name'],
-                    created = bucket['CreationDate'],
+def listv2():
+        client = boto3.client('s3',
+        aws_access_key_id=get_main_aws_access_key(),
+        aws_secret_access_key=get_main_aws_secret_key()
+        endpoint_url='http://172.16.115.75:8080',
+        region_name='',
         )
-"""
-print response1
+        bucket = client.create_bucket(Bucket='sts_bkt2')
+        response1 = client.list_objects_v2(Bucket='sts_bkt2')
+        print response1
+
+

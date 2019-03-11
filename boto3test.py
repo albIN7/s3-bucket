5f98af3c4d2a3edb1f2bf6a874a4812ab764620d
Albin Antony <aantony@redhat.com>
2019-03-11 16:10:51 +0530

    Update boto3test.py

--- boto3test.py
+++ boto3test.py
@@ -1,24 +1,22 @@
 import boto3
-import hashlib
-import boto3.s3.connection
+import json
+import logging
 
-access_key = 'TESTER'
+logging.basicConfig(filename="boto.log", level=logging.DEBUG)
+access_key = 'TESTER1'
 secret_key = 'test123'
-
-print("Before creating connection")
-
-conn = boto3.connect_s3(
-aws_access_key_id = access_key,
-aws_secret_access_key = secret_key,
-port = 8000,
-host = 'localhost',
-is_secure=False,
-validate_certs=False,
-calling_format = boto3.s3.connection.OrdinaryCallingFormat(),
-)
-
-print "After creating connection"
-
-bucket = conn.create_bucket(Bucket ='my-new-shiny-bucket')
-response = conn.list_objects_v2(Bucket = 'my-new-shiny-bucket')
-print response
+s3client = boto3.client('s3',
+aws_access_key_id = '0555b35654ad1656d804',
+#aws_access_key_id = 'TESTER1',
+aws_secret_access_key = 'h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q==' ,
+#aws_secret_access_key = 'test123',
+endpoint_url='http://s3.us-east.localhost:8000',
+#endpoint_url='http://10.8.128.43:8080',
+region_name='',)
+#print "After creating S3 connection"
+bucket_name = 'my-bucket'
+s3bucket = s3client.create_bucket(Bucket=bucket_name)
+for j in range(5):
+	s3client.put_object(Bucket=bucket_name, Key=str(j))
+response = s3client.list_objects_v2(Bucket=bucket_name)
+print(response)
